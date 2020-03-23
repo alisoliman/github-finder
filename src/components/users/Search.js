ad3f48a123eca7fcc2961fa1568d750d359fc68b
Ali <ali.soliman95@gmail.com>
2020-03-23 22:54:34 +0200

    Create a new Context for Alerts, Add import for env files to prep for deployment

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,15 +1,17 @@
 import React, {useContext, useState} from 'react';
-import PropTypes from 'prop-types'
 import GithubContext from '../../context/github/githubContext'
+import AlertContext from '../../context/alert/alertContext'
 
-const Search = ({setAlert}) => {
+const Search = () => {
     const githubContext = useContext(GithubContext);
+    const alertContext = useContext(AlertContext);
     const [text, setText] = useState('');
+
     const onChange = (e) => setText(e.target.value);
     const onSubmit = (e) => {
         e.preventDefault();
         if (text === '') {
-            setAlert('Please enter something', 'danger')
+            alertContext.setAlert('Please enter something', 'danger')
         } else {
             githubContext.searchUsers(text);
             setText('');
@@ -29,7 +31,4 @@ const Search = ({setAlert}) => {
     );
 };
 
-Search.propTypes = {
-    setAlert: PropTypes.func.isRequired,
-};
 export default Search;
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,9 +1,19 @@
 import React, {useReducer} from "react";
 import GithubContext from './githubContext';
-import GithubReducer from './GithubReducer';
+import GithubReducer from './githubReducer';
 import {CLEAR_USERS, GET_REPOS, GET_USER, SEARCH_USERS, SET_LOADING} from "../types";
 import axios from "axios";
 
+let githubClientID;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+    githubClientID = process.env.REACT_APP_GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+    githubClientID = process.env.GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
 
 const GithubState = props => {
     const initialState = {
@@ -18,7 +28,7 @@ const GithubState = props => {
     // Search Users
     const searchUsers = async (text) => {
         setLoading();
-        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${githubClientID}&client_secret=${githubClientSecret}`);
         dispatch({
             type: SEARCH_USERS,
             payload: res.data.items,
@@ -28,7 +38,7 @@ const GithubState = props => {
     // Get User
     const getUser = async (username) => {
         setLoading();
-        const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        const res = await axios.get(`https://api.github.com/users/${username}?client_id=${githubClientID}&client_secret=${githubClientSecret}`);
         dispatch({
             type: GET_USER,
             payload: res.data
@@ -38,7 +48,7 @@ const GithubState = props => {
     // Get Repos
     const getUserRepos = async (username) => {
         setLoading();
-        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientID}&client_secret=${githubClientSecret}`);
         dispatch({type: GET_REPOS, payload: res.data});
     };
 
